187382592fce11c678db2ba6bdbb042cff1582a9
ArturKuzmich <Artur1Kuzmich@gmail.com>
2024-02-14 16:49:51 +0200

    locale for auth

--- i18n.config.ts
+++ i18n.config.ts
@@ -3,7 +3,7 @@ import ua from './locales/ua.json'
 
 export default {
   legacy: false,
-  locale: 'uk-UK',
+  locale: 'uk-UK', // default locale
   locales: ['uk-UK', 'en-US'],
   messages: {
     'en-US': en,
--- pages/auth/sign-in.vue
+++ pages/auth/sign-in.vue
@@ -1,48 +1,49 @@
 <script setup lang="ts">
 import type { FormError, FormSubmitEvent } from '#ui/types'
-
 const store = useGlobalStore()
+const { t } = useI18n()
 
 const form = reactive({
   phone: undefined,
   password: undefined,
 })
 
-function validate(state: any): FormError[] {
+function validate(): FormError[] {
   const errors = []
-  if (!state.phone)
-    errors.push({ path: 'phone', message: 'Required' })
-  if (!state.password)
-    errors.push({ path: 'password', message: 'Required' })
+  if (!form.phone)
+    errors.push({ path: 'phone', message: t('formValidation.required') })
+  if (!form.password)
+    errors.push({ path: 'password', message: t('formValidation.required') })
   return errors
 }
 
 async function onSubmit(event: FormSubmitEvent<any>) {
   store.login(event.data)
 }
+
 </script>
 
 <template>
   <div class="self-center w-full lg:w-1/2">
     <h1 class="text-2xl text-gray-700 mb-4">
-      Авторизуватись
+      {{ $t('signIn.form.title') }}
     </h1>
     <UForm
-      :validate="validate"
+        :validate="validate"
       :state="form"
       class="space-y-4"
       @submit="onSubmit"
     >
       <UFormGroup
-        label="Phone"
-        name="phone"
+          :label="$t('signIn.form.labels.email')"
+        name="email"
       >
         <UInput v-model="form.phone" />
       </UFormGroup>
 
       <UFormGroup
-        label="Password"
-        name="password"
+          :label="$t('signIn.form.labels.password')"
+          name="password"
       >
         <UInput
           v-model="form.password"
@@ -54,8 +55,23 @@ async function onSubmit(event: FormSubmitEvent<any>) {
         type="submit"
         size="lg"
       >
-        Submit
+        {{ $t('signIn.form.labels.submit') }}
       </UButton>
     </UForm>
+    <div class="flex align-center justify-between mt-4">
+      <ULink
+          to="/auth/sign-up"
+          inactive-class="   hover:text-violet-400"
+      >
+        {{ $t('signIn.form.forgot') }}
+      </ULink>
+      <ULink
+          to="/auth/sign-up"
+          inactive-class=" hover:text-violet-400 "
+      >
+        {{ $t('signIn.form.signUp') }}
+      </ULink>
+    </div>
+
   </div>
 </template>
