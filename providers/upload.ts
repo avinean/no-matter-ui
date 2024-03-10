24f515f3ee12d0f5c1a54e418b35c903e83d1959
Vladyslav Lubeznyy <vladyslavlubeznyy@MacBook-Pro.local>
2024-03-10 18:54:45 +0200

    update image provider

--- providers/upload.ts
+++ providers/upload.ts
@@ -1,8 +1,6 @@
 import { joinURL } from 'ufo'
 import type { ProviderGetImage } from '@nuxt/image'
 
-export const getImage: ProviderGetImage = (src) => {
-  return {
-    url: joinURL(process.env.IMAGE_BASE_URL!, src),
-  }
-}
+export const getImage: ProviderGetImage = src => ({
+  url: joinURL(`${process.env.IMAGE_BASE_URL || ''}/uploads`, src),
+})
