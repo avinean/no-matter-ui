a9845d519f096673ed84b5da26672af2c7ef9646
Vladyslav Lubeznyy <vladyslavlubeznyy@MacBook-Pro.local>
2024-03-10 20:13:39 +0200

    update env data

--- providers/upload.ts
+++ providers/upload.ts
@@ -2,5 +2,5 @@ import { joinURL } from 'ufo'
 import type { ProviderGetImage } from '@nuxt/image'
 
 export const getImage: ProviderGetImage = src => ({
-  url: joinURL(`${process.env.IMAGE_BASE_URL || ''}/uploads`, src),
+  url: joinURL(`${process.env.IMAGE_BASE_URL || '/uploads'}`, src),
 })
